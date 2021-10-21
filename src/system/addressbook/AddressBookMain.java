033620d58da4b685987e87449821adb3deeda3e1
ShubhamB2595 <shubhambhele95@gmail.com>
2021-10-21 14:06:20 +0530

    Welcome to Address Book Program

---
+++ src/system/addressbook/AddressBookMain.java
@@ -0,0 +1,11 @@
+package system.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to address book program");
+	}
+
+}
